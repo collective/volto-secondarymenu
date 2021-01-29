9714dbb286c06e0b02abfde20b04e35b6c9457e7
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-01-29 15:02:05 +0100

    fixed index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { secondaryMenuReducer } from './reducers';
 import SecondaryMenuConfigurationWidget from './widget/SecondaryMenuConfigurationWidget';
 import { getSecondaryMenu } from './actions';
-import SecondaryMenu from './components';
+import SecondaryMenu from './components/SecondaryMenu';
 
-export { SecondaryMenuConfigurationWidget, getSecondaryMenu };
+export { SecondaryMenuConfigurationWidget, getSecondaryMenu, SecondaryMenu };
 
 export default (config) => {
   config.widgets.id = {
     ...config.widgets.id,
-    //secondary_menu_configuration: SecondaryMenuConfigurationWidget,
+    secondary_menu_configuration: SecondaryMenuConfigurationWidget,
   };
 
   config.addonReducers = {
